25567989e28607673c3d3250b578932f0308de47
TRUNG <ductrungkhtn@gmail.com>
2014-05-11 15:50:48 +0200

    first version of K-meadian clustering algorithms. It works well with single user, but not for all users.

--- LocationService/src/main/scala/Test.scala
+++ LocationService/src/main/scala/Test.scala
@@ -1,4 +1,4 @@
-//package test
+package main.scala.test
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
@@ -11,7 +11,7 @@ import scala.collection.immutable.Seq;
 
 object Test {
     
-    val TIME_INTERVAL = 120 // 15 minutes
+    val TIME_INTERVAL = 60 // minutes
     val TOP_K = 3
     
     def main(args: Array[String]): Unit = {
@@ -20,9 +20,10 @@ object Test {
             .setAppName("test location service")
 
         val context = new SparkContext(conf)
-        //val input = "/Users/loveallufev/Documents/MATLAB/mobile-locations-training.txt"
+        val input = "/Users/loveallufev/Documents/MATLAB/mobile-locations-training.txt"
         //val input = "/Users/loveallufev/Documents/MATLAB/user/training-user21.txt"
-        val input = "/Users/loveallufev/Documents/MATLAB/user/smalltraining21.txt"
+        //val input = "/Users/loveallufev/Documents/MATLAB/user/smalltraining21.txt"
+        //val input = "/Users/loveallufev/Documents/MATLAB/fakedata"
 
         val data = context.textFile(input, 1)
 
@@ -55,37 +56,60 @@ object Test {
                 var max_duration = -1 // duration which user is in a region
                 var best_cellID: Int = 0 // cellID which user is in for the longest time
                 var last_cellID: Int = -1
-
+                
+                var mapLocationToTotalTime = HashMap[Int, Int]()
                 for ((time, cellID) <- sequence) {
-
+                    
                     val T = if (time < base) time else base
-
-                    if (T - lasttime > max_duration) {
-                        max_duration = T - lasttime
-                        best_cellID = last_cellID
+                    
+                    var timeOfTheLastLocation = mapLocationToTotalTime.get(last_cellID) match {
+                        case Some(temp) => temp
+                        case None => 0
                     }
+                    
+                    mapLocationToTotalTime = mapLocationToTotalTime.+(last_cellID -> (timeOfTheLastLocation + T - lasttime))
+                    
+                    if (T == base)
+                    {
+                        
+                        var maxTime = -1
+                        var bestCellID = -1
+                        // update the value for this interval
+                        mapLocationToTotalTime.foreach {
+                            case (key, value) => {
+                                if (value > maxTime 
+                                        && key != 1	// remove case of lost signal
+                                        ) {
+                                    maxTime = value
+                                    bestCellID = key
+                                }
+                            }
+                        }
 
-                    if (T == base) { // it means time >= base
-                        // update the value of lastIndex
                         lastIndex = lastIndex + 1
-                        discreteValues.update(lastIndex, best_cellID)
+                        discreteValues.update(lastIndex, bestCellID)
 
-                        // update max_duration is the duration of the first location of this interval
-                        max_duration = time - base // calculate the max duration for next interval
-                        while (max_duration >= TIME_INTERVAL) {
+                        mapLocationToTotalTime = HashMap[Int, Int]()
+                        // this statement only update the time of last_cellID
+                        mapLocationToTotalTime = mapLocationToTotalTime.+(last_cellID -> (time - base))
+                        base = base + TIME_INTERVAL
+
+                        while (base <= time) {
                             lastIndex = lastIndex + 1
                             discreteValues.update(lastIndex, last_cellID)
 
-                            base = base + TIME_INTERVAL // move to next interval
-                            max_duration = time - base // calculate the max duration for next interval	
+                            // this statement only update the time of last_cellID
+                            mapLocationToTotalTime = mapLocationToTotalTime.+(last_cellID -> (time - base))
+                            base = base + TIME_INTERVAL
                         }
-                        base = base + TIME_INTERVAL
                     }
-
-                    lasttime = time
+                    
                     last_cellID = cellID
+                    lasttime = time
+                    
                 }
-
+                
+                
                 while (lastIndex < discreteValues.length -1) {
                     lastIndex = lastIndex + 1
                     discreteValues.update(lastIndex, last_cellID)
@@ -93,36 +117,78 @@ object Test {
                     lasttime = lasttime + TIME_INTERVAL // move to next interval
                 }
                 //println("time:%s sequences:%s".format((userId, year, month, day), discreteValues.mkString(",")))
-                discreteValues.map(x => (x,1))	//[(dimesion1,fre1), (dimension2,fre2), ..., (dimensionN, freN)]
+                
+                ((userId, year, month), (day, discreteValues.map(x => (x,1))))	//[(dimesion1,fre1), (dimension2,fre2), ..., (dimensionN, freN)]))
+                //discreteValues.map(x => (x,1))	//[(dimesion1,fre1), (dimension2,fre2), ..., (dimensionN, freN)]
                 // use it for select median
             }
         }
-
-
-        println("After transfrom data to time-series data:")
-        timeSeries.collect.foreach(x => println(x.mkString(",")))
         
-        val K = 100//00
         
-	    var points = timeSeries.takeSample(false, K, 42)
-	    println("sample points")
-	    points.foreach(x => println(x.mkString(",")))
+        // Auto update the last location of the the first interval in each day
+        
+        val dataOfMonthsSorted = timeSeries.groupByKey.flatMap {
+            monthlyData => {
+                
+	                var sortedData = monthlyData._2.sortBy(_._1)	// sort asc by day
+	                var lastCellID = -1
+	                var lastday = -1
+	                println("New month" + monthlyData._1._3)
 
-	    var kPoints = new HashMap[Int, Array[(Int, Int)]]	// (centroisIndex -> N_dimension_point : [(di1, frenquency), (di2,fre)...])
-	    val iterations = 40
+                    sortedData.foreach {
+                        case (day, locationsInThisDay) => {
+                            if (day - lastday == 1) {
+                                //println("old data: %s of day %d".format(locationsInThisDay.mkString(","), day))
+                                val length = locationsInThisDay.length
+                                var i = 0
+                                while (i < length && locationsInThisDay(i)._1 == -1) {
+                                    locationsInThisDay.update(i, (lastCellID, 1))
+                                    i = i + 1
+                                }
+                                lastCellID = locationsInThisDay(length - 1)._1
+                                lastday = day
+                                //println("new data: %s of day %d".format(locationsInThisDay.mkString(","), day))
+                            } else {
+                                lastday = -1
+                                lastCellID = -1
+                            }
+
+                        }
+	                }
+	                sortedData.map (x => x._2)
+            }
+        }
+        
+        
+        //
+
+
+        //println("After transfrom data to time-series data:")
+        //dataOfMonthsSorted.collect.foreach(x => println(x.mkString(",")))
+        
+        val K = 400//00
+        
+	    var points = dataOfMonthsSorted.takeSample(false, K, 42)
+	    //println("sample points")
+	    //points.foreach(x => println(x.mkString(",")))
+
+	    var kPoints = new Array[(Int, Array[(Int, Int)])](K)	// (centroisIndex -> N_dimension_point : [(di1, frenquency), (di2,fre)...])
+	    val iterations = 30
 	   
 	    for (i <- 0 to points.size - 1) {
-	        kPoints = kPoints.+(i -> points(i))
+	        kPoints.update(i,(i,points(i)))
 	    }
         
-        var modelsData = new HashMap[Int, Array[Array[(Int, Int)]]]()
+        var modelsData = Array[(Int, Array[Array[(Int, Int)]])]()	// (modelIndex, matrix of location candidate of each time)
 	   
 	    for (i <- 1 to iterations) {
-	      val  closest = timeSeries.map ( p => (findClosetCenter(p, kPoints)(distanceFunction), p) )
+	        println("Iter " + i)
+	      val  closest = dataOfMonthsSorted.map ( p => (findClosetCenter(p, kPoints)(distanceFunction), p) )
 	      
-	      closest.collect.foreach(println)
+	      //closest.collect.foreach(println)
 	      
-	      val abcxyz = closest.flatMap{
+	      // find location candidate of each interval in each cluster
+	      val locationCandidates = closest.flatMap{
 	           case (centerIndex, coordinationOfPoint) => {
 	                 var indexes = (0 to coordinationOfPoint.length -1)
 	                 
@@ -132,13 +198,15 @@ object Test {
 	      }.reduceByKey(_ + _)	// count how many time user is in this location at this time Interval (per cluster)
 	      .map{ 
 	           case ((centerIndex, intervalIndex, location), frequency) => ((centerIndex, intervalIndex), (location, frequency))
-	           }.groupByKey
+	      }.groupByKey
 	           
-	      abcxyz.collect.foreach(println)
-	      var closetDistribution = abcxyz.map{
+	      //abcxyz.collect.foreach(println)
+	      
+	      var topNLocationCandidateOfEachTimeInEachCluster = locationCandidates.map{
 	           case ((centerIndex, intervalIndex), sequenceOfLocationFrequency) => {
 	                 var topLocations = (sequenceOfLocationFrequency.sortBy(x => -x._2)// sort desc by frequency
 	                         .take(TOP_K))
+	                 //println("Get top of index:" + centerIndex)
 	                 (centerIndex, (intervalIndex, topLocations))
 	           }
 	      }.groupByKey.map{
@@ -147,29 +215,25 @@ object Test {
 	           }
 	      }
 	       
+	       //println("Old kPoints:")
+	       //kPoints.foreach {case (key, value) => println("%d -> %s".format(key, value.mkString(",")))}
+	       println("Number of cluster:" + topNLocationCandidateOfEachTimeInEachCluster.count)
+	       //kPoints =  kPoints.empty
 	       // update models
-	      closetDistribution.collect.foreach{
-	           case (centerIndex, matrix) => {
-	                 println("index %d matrix : %s".format(centerIndex, matrix))
-	                 modelsData = modelsData.updated(centerIndex, matrix)
-	                  // = modelsData.+ (centerIndex -> matrix)
-	                 
-	                 
-	                 // update centers
-	                 
-	                 val length = matrix.length
-	                 var temp = matrix.map (x => x(0)).toArray
-	                 /*
-	                     new Array[(Int, Int)](length)
-	                 println("length = " + length)
-	                 for (j <- 0 to length-1){
-	                     temp.update(j, matrix(j)(0))
-	                 }
-	                 * 
-	                 */
-	                 kPoints = kPoints.+ (centerIndex -> temp)
-	           }
-	      }
+	       
+	       
+	       modelsData = topNLocationCandidateOfEachTimeInEachCluster.map {
+	             case (centerIndex, matrix) => (centerIndex -> matrix)
+	       }.toArray
+	       
+	       kPoints = topNLocationCandidateOfEachTimeInEachCluster.map {
+	             case (centerIndex, matrix) => {
+	                 (centerIndex, matrix.map (x => x(0)).toArray)
+	             }
+	       }.toArray
+
+	         //println("New kPoints:")
+	         //kPoints.foreach {case (key, value) => println("%d -> %s".format(key, value.mkString(",")))}
 	    }
 	    
         println("KMEAN - OK ----------------------------------------")
@@ -183,8 +247,11 @@ object Test {
             }
         }.toArray
         
-        models.foreach (x => { x.printInfo; println("----------------\n") } )
+        
 
+        println("Number of models: %d".format(models.length))
+        
+        models.foreach (x => { x.printInfo; println("----------------\n") } )
 	    //kPoints.foreach(x => {
 	    //    println("%d -> (%s)".format(x._1, x._2.mkString(",")))
 	    //})
@@ -195,7 +262,17 @@ object Test {
 	    previousPos.update(1, (10*60+26, 548))
 	    previousPos.update(2, (12*60 + 49, 587))
 	    
-	    println("Predict:%s".format(predict(previousPos, 18*60+24, models.toVector).mkString(",")))
+	    println("Predict:%s\n\n".format(predict(previousPos, 18*60+24, models.toVector).mkString(",")))
+	    
+	    previousPos.update(0, (5*60+33, 550))
+	    previousPos.update(1, (9*60+48, 548))
+	    previousPos.update(2, (16*60 + 32, 548))
+	    println("Predict:%s\n\n".format(predict(previousPos, 19*60+8, models.toVector).mkString(",")))
+	    
+	    previousPos.update(0, (13*60 + 30, 587))
+	    previousPos.update(1, (15*60 + 30, 598))
+	    previousPos.update(2, (18*60 + 15, 549))
+	    println("Predict:%s\n\n".format(predict(previousPos, 19*60 + 33, models.toVector).mkString(",")))
 
     }
     
@@ -203,13 +280,12 @@ object Test {
         val length = x.length
 	           var distance : Double = 0
 	           for (j <- 0 to length-1){
-	                 distance = distance + (x(j)._1 - y(j)._1)*(x(j)._1 - y(j)._1)
-	                 
+	                 distance = distance + (x(j)._1 - y(j)._1)*(x(j)._1 - y(j)._1)    
 	           }
 	    distance
     }
     
-    def findClosetCenter[T]( point: T, centers: HashMap[Int, T])(distanceFunction : (T,T) => Double) : Int = {
+    def findClosetCenter[T]( point: T, centers: Array[(Int, T)])(distanceFunction : (T,T) => Double) : Int = {
         var closetCenterID = -1
         var minDistance = distanceFunction(centers.head._2, point) + 1
         centers.foreach {
@@ -229,12 +305,12 @@ object Test {
             ) : Array[(Int, Double)] = {	// Array of (time, Location) ==> Next location
     	var bestModelID = -1
     	var bestModel : LocationModel = null
-    	var maxProb : Double = 0
+    	var maxProb : Double = -1
     	
         models.foreach (model => {
             
             var prob = model.calculateProb(previousPos) 
-            println("model:%d prob:%f".format(model.modelID, prob))
+            println("model:%d prob:%f predict:%s".format(model.modelID, prob, model.predict(nextTime).mkString(",")))
     		  if (prob > maxProb){
     		       maxProb = prob
     		       bestModelID = model.modelID
@@ -246,7 +322,7 @@ object Test {
     		
     		
     	
-    	println("bestModelID=%d MaxProb=%f".format(bestModelID, maxProb))
+    	println("bestModelID=%d MatchingPercentage=%f".format(bestModelID, maxProb))
     	bestModel.predict(nextTime)
     	
     }
@@ -265,7 +341,7 @@ class LocationModel(val TIME_INTERVAL : Int) {
             locationCandidates => {
                 var sum = locationCandidates.foldLeft(0)((x,y) => (x + y._2))
                 var temp = locationCandidates.sortBy{ case (location, freq) => location }
-                temp.map { case (loc, freq) => (loc, freq*1.0/sum) }.toArray
+                temp.map { case (loc, freq) => (loc, freq*1.0/sum) }.toArray.sortBy(x => -x._2)// sort desc by probability
             }
         }.toArray
     }
